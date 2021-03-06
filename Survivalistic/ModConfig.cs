f00552b15eb78bbb35c5864995f2fbb51a4b8d32
Gustavo Hinkel <78951385+Ophaneom@users.noreply.github.com>
2021-03-06 12:16:36 -0300

    Changed location archives

---
+++ Survivalistic/ModConfig.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace Survivalistic
+{
+    class ModConfig
+    {
+        public string Language { get; set; }
+        public string Position { get; set; }
+        public int CustomPositionX { get; set; }
+        public int CustomPositionY { get; set; }
+        public Color HungerBarColor { get; set; }
+        public Color HungerSaturationColor { get; set; }
+        public Color ThirstBarColor { get; set; }
+        public Color ThirstSaturationColor { get; set; }
+    }
+}
---
+++ Survivalistic/SaveData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using StardewModdingAPI;
+using StardewValley;
+
+namespace Survivalistic
+{
+    class SaveData
+    {
+        public float hunger { get; set; } = 100;
+        public float thirst { get; set; } = 100;
+        public float hungerSaturation { get; set; } = 50;
+        public float thirstSaturation { get; set; } = 50;
+
+        public void SyncToHost()
+        {
+            if (Context.IsMainPlayer)
+                ModEntry.instance.Helper.Data.WriteSaveData($"spacechase0.AnotherHungerMod.{Game1.player.UniqueMultiplayerID}", ModEntry.data);
+            else
+            {
+                ModEntry.instance.Helper.Multiplayer.SendMessage(this, "MESSAGE_KEY", null, new long[] { Game1.MasterPlayer.UniqueMultiplayerID });
+            }
+        }
+    }
+}
