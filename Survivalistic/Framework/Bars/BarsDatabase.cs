6ea3bf9b888b1f4af9e8cfc4703392ccd5e09eff
Gustavo Hinkel <gustavopradahinkel@gmail.com>
2022-05-26 01:25:39 -0300

    Updated foods database

--- Survivalistic/Framework/Bars/BarsDatabase.cs
+++ Survivalistic/Framework/Bars/BarsDatabase.cs
@@ -2,14 +2,14 @@
 {
     public class BarsDatabase
     {
-        public static float hunger_velocity = 2f;
-        public static float thirst_velocity = 3f;
+        public static float hunger_velocity = 0.5f;
+        public static float thirst_velocity = 1f;
 
         public static bool render_numerical_hunger = false;
         public static bool render_numerical_thirst = false;
 
         public static bool right_side = false;
 
-        public static float tool_use_multiplier = 1;
+        public static float tool_use_multiplier = 0.25f;
     }
 }
